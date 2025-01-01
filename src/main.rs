3f101a457954c98d30c52fd475a5970c598ef4c8
d.drapei <d.drapei@rgu.ac.uk>
2025-01-01 15:04:51 +0000

    string and slices

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 fn main() {
+    // arrays and their slices
     let arr: [i8; 5] = [1, 2, 3, 4, 5];
     let arr_slice: &[i8] = &arr[2..4];
 
 
     println!("slice = {:?}", arr_slice);
+
+    // strings and slices 
+    let mut s = String::from("Hello, World");
+    println!("The string is: {}", s);
+    s.push_str(", Rust ");
+    println!("The string is: {}", s);
+
+    let s: &str = "Hello &str, World";
+    println!("The string slice is: {}", s);
 }
 
